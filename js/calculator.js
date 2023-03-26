c26d60e8bafdb35d9d274a8a55f33e69178eaf7d
Josue_Tejada <omar14tejada@gmail.com>
2023-03-26 15:27:29 -0400

    adding initial styling

--- js/calculator.js
+++ js/calculator.js
@@ -3,27 +3,24 @@ let previous = null;
 let operator = null;
 let operatorClicked = false;
 
-/**
- * Calculates the operation and updates the display.
- */
 function performOperation() {
   let result;
   const current = parseNumber(display.value);
   previous = parseNumber(previous);
 
-  switch(operator) {
-    case '+' :
+  switch (operator) {
+    case "+":
       result = previous + current;
-    break;
-    case '-' :
-        result = previous - current;
-    break;
-    case '*' :
-        result = previous * current;
-    break;
-    case '/' :
-        result = previous / current;
-    break;
+      break;
+    case "-":
+      result = previous - current;
+      break;
+    case "*":
+      result = previous * current;
+      break;
+    case "/":
+      result = previous / current;
+      break;
   }
 
   display.value = result;
@@ -32,10 +29,10 @@ function performOperation() {
 
 /**
  * Parses the display value into a number (float or int).
- * @param {String} num 
+ * @param {String} num
  */
 function parseNumber(num) {
-  return num.includes('.') ? parseFloat(num) : parseInt(num);
+  return num.includes(".") ? parseFloat(num) : parseInt(num);
 }
 
 /**
@@ -50,18 +47,17 @@ function clickOperator(event) {
 
 /**
  * Captures a number click and updates the display value.
- * @param {Event} event 
+ * @param {Event} event
  */
 function clickNumber(event) {
   const val = event.target.value;
 
-  if( operatorClicked ) {
+  if (operatorClicked) {
     display.value = val;
     operatorClicked = false;
   } else {
-    display.value == 0 ? display.value = val : display.value += val;
+    display.value == 0 ? (display.value = val) : (display.value += val);
   }
-
 }
 
 /**
@@ -71,48 +67,26 @@ function clear() {
   display.value = 0;
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+  display = document.getElementById("display");
 
-// add event listener for when the DOM is loaded
-document.addEventListener('DOMContentLoaded', () => {
-
-  // set the variable called display declared at the top of this file equal to the display element
-  // HINT: use its id #display to get a reference to it
-  display = document.getElementById('display');
-
-  // get a reference to all of the numbers
-  // loop over each of the numbers
-  // add a click event listener to each number to call the function clickNumber
-  const allNumbers = document.querySelectorAll('.number');
-  allNumbers.forEach( (num) => {
-    num.addEventListener('click', clickNumber)
-    console.log('num', num);
+  const allNumbers = document.querySelectorAll(".number");
+  allNumbers.forEach((num) => {
+    num.addEventListener("click", clickNumber);
+    console.log("num", num);
   });
 
-  // get a reference to the decimal point button
-  // add a click event listener to call the function clickNumber
-  // the decimal point is part of the number so you can call clickNumber for it 
-  // as you would for a number
-  const decimalBtn = document.querySelector('.decimal');
-  decimalBtn.addEventListener('click', clickNumber);
+  const decimalBtn = document.querySelector(".decimal");
+  decimalBtn.addEventListener("click", clickNumber);
 
+  const allClr = document.querySelector(".all-clear");
+  allClr.addEventListener("click", clear);
 
-  // get a reference to the all clear button
-  // add a click event listener to call the function clear  
-  const allClr = document.querySelector('.all-clear');
-  allClr.addEventListener('click', clear);
-
-  // get a reference to all of the operators;
-  // loop over each of the operators
-  // add a click event listener to each operator to call the function clickOperator
-  const operators = document.querySelectorAll('.operator');
-  operators.forEach( (operator) => {
-    operator.addEventListener('click', clickOperator)
+  const operators = document.querySelectorAll(".operator");
+  operators.forEach((operator) => {
+    operator.addEventListener("click", clickOperator);
   });
 
-  // add click event listener for the equal sign
-  // should call the function performOperation
-  const equality = document.querySelector('.equal-sign');
-  equality.addEventListener('click', performOperation);
-
+  const equality = document.querySelector(".equal-sign");
+  equality.addEventListener("click", performOperation);
 });
-
